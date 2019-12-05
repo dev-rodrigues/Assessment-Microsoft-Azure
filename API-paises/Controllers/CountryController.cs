a73653843ebeb27437bfdb0ecfa150c86dc35f87
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-04 23:50:28 -0300

    classes doida

--- API-paises/Controllers/CountryController.cs
+++ API-paises/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Service.Countries;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -15,5 +16,11 @@ namespace API_PAISES.Controllers {
     [RoutePrefix("api/county")]
     public class CountryController : ApiController {
 
+        
+
+        public IHttpActionResult Index() {
+            
+            return Ok();
+        }
     }
 }
---
+++ Core/Repositories/Countries/ICountry.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Repositories.Countries {
+    public interface ICountry {
+        int Id { get; set; }
+        string Name { get; set; }
+        string URLImage { get; set; }
+    }
+}
