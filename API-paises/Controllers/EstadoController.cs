3366b29d8fcbae6a36feaa61e4414dd14bb45e86
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-05 11:27:39 -0300

    identacao

--- API-paises/Controllers/EstadoController.cs
+++ API-paises/Controllers/EstadoController.cs
@@ -6,38 +6,31 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
-namespace API_estados.Controllers
-{
-    [RoutePrefix("api/Estado")]
-    public class EstadoController : ApiController
-    {
+namespace API_estados.Controllers {
+    [RoutePrefix("api/estado")]
+    public class EstadoController : ApiController {
         [HttpGet]
-        public IHttpActionResult Index()
-        {
+        public IHttpActionResult Index() {
             return Ok();
         }
 
         [HttpGet]
-        public IHttpActionResult Show(int id)
-        {
+        public IHttpActionResult Show(int id) {
             return Ok();
         }
 
         [HttpPost]
-        public IHttpActionResult Store(InputEstadoModel input)
-        {
+        public IHttpActionResult Store(InputEstadoModel input) {
             return Ok();
         }
 
         [HttpPut]
-        public IHttpActionResult Update(InputEstadoModel input)
-        {
+        public IHttpActionResult Update(InputEstadoModel input) {
             return Ok();
         }
 
         [HttpDelete]
-        public IHttpActionResult Destroy(int id)
-        {
+        public IHttpActionResult Destroy(int id) {
             return Ok();
         }
     }
