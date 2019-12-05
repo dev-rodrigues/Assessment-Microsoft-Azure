c36bdaec22da20b6e062f94bfcbd065c197d6ca1
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-04 22:44:46 -0300

    repositorio generico

---
+++ Data/Repository/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository {
+    public class GenericRepository<T> where T : class {
+
+        public T Salvar(T objeto) {
+            throw new NotImplementedException();
+        }
+
+        public T Editar(T objeto) {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id) {
+            throw new NotImplementedException();
+        }
+
+        public T BuscarPorId(int id) {
+            throw new NotImplementedException();
+        }
+
+        public List<T> ListarPorId(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
