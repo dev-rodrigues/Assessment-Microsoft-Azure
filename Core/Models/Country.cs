0e2414bbaae735d98b9ba60b0b4b99c3b6a37c39
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-05 00:42:03 -0300

    repository

--- Core/Models/Country.cs
+++ Core/Models/Country.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 
 namespace Core.Models {
     public class Country {
-
         public int Id { get; set; }
         public string Name { get; set; }
         public string URLImage { get; set; }
