cfeaec4856518e8414747a77491e27d59f356224
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-04 23:07:30 -0300

    controlador

---
+++ API-paises/Controllers/CountryController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.UI.WebControls;
+
+namespace API_PAISES.Controllers {
+
+    [RoutePrefix("api/county")]
+    public class CountryController : ApiController {
+
+    }
+}
---
+++ API-paises/Models/InputModel/InputCountryModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API_PAISES.Models.InputModel {
+    public class InputCountryModel {
+        public string Name { get; set; }
+        public string IdImage { get; set; }
+    }
+}
