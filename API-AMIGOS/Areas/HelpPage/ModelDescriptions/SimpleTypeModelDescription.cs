4e58f538a9763d6ccd63ca2ea374b01d34dea05a
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-04 22:02:24 -0300

    apagando pasta maluca

--- API-AMIGOS/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace API_AMIGOS.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- API-AMIGOS/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="API_AMIGOS.WebApiApplication" Language="C#" %>
