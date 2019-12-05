98c7e8e75ec68102b5ea0aae0daaf247bf1daa1e
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-04 22:16:38 -0300

    rename

--- Core/Models/Country.cs
+++ Core/Models/Country.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Core.Models {
-    public class Countries {
+    public class Country {
 
         public int Id { get; set; }
         public string Name { get; set; }
