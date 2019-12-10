baa177f2c745e42e49d09205a5cd67751e869ca9
carlos henrique <carlos.hrq.rodrigues@gmail.com>
2019-12-10 15:33:58 -0300

    migration

---
+++ Application/Migrations/201912101821178_update.cs
@@ -0,0 +1,26 @@
+﻿namespace Application.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class update : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.States", "CountryId", "dbo.Countries");
+            DropIndex("dbo.States", new[] { "CountryId" });
+            AlterColumn("dbo.States", "CountryId", c => c.Int());
+            CreateIndex("dbo.States", "CountryId");
+            AddForeignKey("dbo.States", "CountryId", "dbo.Countries", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.States", "CountryId", "dbo.Countries");
+            DropIndex("dbo.States", new[] { "CountryId" });
+            AlterColumn("dbo.States", "CountryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.States", "CountryId");
+            AddForeignKey("dbo.States", "CountryId", "dbo.Countries", "Id", cascadeDelete: true);
+        }
+    }
+}
